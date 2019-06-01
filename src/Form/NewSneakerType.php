1ae3a3361c1f7af78242094033d82248b3fafa7d
linux <borja.arcos13@gmail.com>
2019-06-01 13:34:17 +0200

    tallas

---
+++ src/Form/NewSneakerType.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linux
+ * Date: 01/06/19
+ * Time: 13:31
+ */
+
+namespace App\Form;
+
+use App\Entity\Sizes;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+class NewSneakerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // For the full reference of options defined by each form field type
+        // see https://symfony.com/doc/current/reference/forms/types.html
+        // By default, form fields include the 'required' attribute, which enables
+        // the client-side form validation. This means that you can't test the
+        // server-side validation errors from the browser. To temporarily disable
+        // this validation, set the 'required' attribute to 'false':
+        // $builder->add('title', null, ['required' => false, ...]);
+        $builder
+            ->add('price', null, [
+                'attr' => ['rows' => 20, 'class'=>'form-control'],
+                'label' => 'Precio'
+            ])
+            ->add('size', ChoiceType::class,[
+                'choices' => [
+                    '38' => "38",
+                    '39' => "39",
+                    '40' => "40",
+                    '41' => "41",
+                    '42' => "42",
+                    '43' => "43",
+                    '44' => "44",
+                    '45' => "45",
+                    '46' => "46",
+                    '47' => "47",
+                    '48' => "48"
+                ],
+
+            ])
+
+        ;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sizes::class,
+        ]);
+    }
+}
