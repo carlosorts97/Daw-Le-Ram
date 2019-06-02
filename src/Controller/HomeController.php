6875062bf5a3bd28d6fa5b7982631172091e6ebe
carlosorts97 <carlosorts97@gmail.com>
2019-06-03 00:09:18 +0200

    ff

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,13 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Cities;
+use App\Entity\Countries;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 class HomeController extends AbstractController
 {
@@ -131,5 +136,205 @@ class HomeController extends AbstractController
             'snkHype' => $snkHype,
             'snkLast' => $snkLast
         ]);
+    }
+
+    /**
+     * @Route("/hola/category",name="app_categoryInsert")
+     */
+    public function InsertIntoCategory (){
+        $category=new Category();
+        $category->setName("Camisetas");
+        $category2=new Category();
+        $category2->setName("Sudaderas");
+        $category3=new Category();
+        $category3->setName("Pantalones");
+        $category4=new Category();
+        $category4->setName("Sneakers");
+        $category5=new Category();
+        $category5->setName("Accesorios");
+        dump($category);
+        dump($category2);
+        dump($category3);
+        dump($category4);
+        dump($category5);
+
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->persist($category);
+        $entityManager->persist($category2);
+        $entityManager->persist($category3);
+        $entityManager->persist($category4);
+        $entityManager->persist($category5);
+        $entityManager->flush();
+
+
+
+        //render the form
+        return $this->render('home/home.html.twig');
+
+    }
+    /**
+     * @Route("/hola/brand",name="app_brandInsert")
+     */
+    public function InsertIntobrand (){
+        $category=new Category();
+        $category->setName("Supreme");
+        $category2=new Category();
+        $category2->setName("Palace");
+        $category3=new Category();
+        $category3->setName("Nike");
+        $category4=new Category();
+        $category4->setName("Adidas");
+        $category5=new Category();
+        $category5->setName("Off-White");
+        $category6=new Category();
+        $category6->setName("Heron preston");
+        $category7=new Category();
+        $category7->setName("Kith");
+        $category8=new Category();
+        $category8->setName("Kaws");
+        $category9=new Category();
+        $category9->setName("Yeezy");
+        $category10=new Category();
+        $category10->setName("Air jordan");
+        $category11=new Category();
+        $category11->setName("Stussy");
+        $category12=new Category();
+        $category12->setName("Bape");
+        $category13=new Category();
+        $category13->setName("Billionaire boys club");
+        $category14=new Category();
+        $category14->setName("Gucci");
+        $category15=new Category();
+        $category15->setName("Louis vuitton");
+
+        dump($category);
+        dump($category2);
+        dump($category3);
+        dump($category4);
+        dump($category5);
+        dump($category6);
+        dump($category7);
+        dump($category8);
+        dump($category9);
+        dump($category10);
+        dump($category11);
+        dump($category12);
+        dump($category13);
+        dump($category14);
+        dump($category15);
+        die();
+
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->persist($category);
+        $entityManager->persist($category2);
+        $entityManager->persist($category3);
+        $entityManager->persist($category4);
+        $entityManager->persist($category5);
+        $entityManager->flush();
+
+
+
+        //render the form
+        return $this->render('home/home.html.twig');
+    }
+
+    /**
+     * @Route("/hola/admin",name="app_adminInsert")
+     */
+    public function InsertIntoadmin (){
+        $user_admin=new User();
+        $user_admin->setName("Admin");
+        $user_admin->setPassword("1234");
+        $user_admin->setRoles(['ROLE_USER']);
+        $user_admin->setAddress("Calle Admin");
+        $user_admin->setEmail("admin@gmail.com");
+        $user_admin->setBirthday(new \DateTime());
+        $user_admin->setPlainPassword("1234");
+        $user_admin->setUsername("admin");
+        $user_admin->setCity($this->getDoctrine()->getRepository(Cities::class)->find("1"));
+
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->persist($user_admin);
+
+        $entityManager->flush();
+
+
+
+        //render the form
+        return $this->render('home/home.html.twig');
+
+    }
+
+    /**
+     * @Route("/hola/pais",name="app_paisInsert")
+     */
+    public function InsertIntoPaises (){
+        $pais=new Countries();
+        $pais->setName("España");
+
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->persist($pais);
+
+        $entityManager->flush();
+
+
+
+        //render the form
+        return $this->render('home/home.html.twig');
+
+    }
+
+    /**
+     * @Route("/hola/ciudad",name="app_ciudadInsert")
+     */
+    public function InsertIntoCiudades (){
+        $ciudad=new Cities();
+        $ciudad->setName("Cataluña");
+        $ciudad->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad2=new Cities();
+        $ciudad2->setName("Madrid");
+        $ciudad2->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad3=new Cities();
+        $ciudad3->setName("Galicia");
+        $ciudad3->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad4=new Cities();
+        $ciudad4->setName("Valencia");
+        $ciudad4->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad5=new Cities();
+        $ciudad5->setName("Andalucia");
+        $ciudad5->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad6=new Cities();
+        $ciudad6->setName("País Vasco");
+        $ciudad6->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad7=new Cities();
+        $ciudad7->setName("Extremadura");
+        $ciudad7->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $ciudad8=new Cities();
+        $ciudad8->setName("Murcia");
+        $ciudad8->setCountry($this->getDoctrine()->getRepository(Countries::class)->find("1"));
+
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->persist($ciudad);
+        $entityManager->persist($ciudad2);
+        $entityManager->persist($ciudad3);
+        $entityManager->persist($ciudad4);
+        $entityManager->persist($ciudad5);
+        $entityManager->persist($ciudad6);
+        $entityManager->persist($ciudad7);
+        $entityManager->persist($ciudad8);
+        $entityManager->flush();
+
+
+
+        //render the form
+        return $this->render('home/home.html.twig');
+
     }
 }
